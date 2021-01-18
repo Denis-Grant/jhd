232a1d7cba7680fe7c6fc98d53687ea3ba34fce0
Apple <apple@MacbookPro.local>
2021-01-18 23:34:06 +0000

    nav

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const navLogo = document.querySelector('.nav__logo');
 const nav = document.querySelector('nav');
-const logoText = document.querySelector('.logo-text');
+const logoText = document.querySelector('.nav__text');
 
 window.addEventListener('scroll', ()=>{
     if (window.pageYOffset > 25){
         // nav.style.transition = 'background-color 500ms ease-in-out';
-        nav.style.backgroundColor = '#eee9e6';
-        nav.style.color = 'black';
+        nav.style.backgroundColor = '#fff';
+        nav.style.color = '#83502e';
         navLogo.src = 'img/logo-sm-black.jpg';
-        logoText.style.color ='#1a1a1a';
+        logoText.style.color ='#83502e';
 
     }
     if (window.pageYOffset < 26) {
@@ -16,6 +16,6 @@ window.addEventListener('scroll', ()=>{
         nav.style.backgroundColor = 'rgba(0,0,0,0)';
         nav.style.color = '';
         navLogo.src = 'img/logo-sm-white.jpg';
-        logoText.style.color ='#fff';
+        logoText.style.color ='#ffffffad';
     }
 });
