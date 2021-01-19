987fc41737fa4131f48e8c6b65c87f90b3057d0a
Apple <apple@MacbookPro.local>
2021-01-19 10:50:53 +0000

    header & nav

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
 const navLogo = document.querySelector('.nav__logo');
 const nav = document.querySelector('nav');
 const logoText = document.querySelector('.nav__text');
+const logoTextLg = document.querySelector('li.nav__text');
 
 window.addEventListener('scroll', ()=>{
-    // if (window.pageYOffset > 25){
-    //     // nav.style.transition = 'background-color 500ms ease-in-out';
-    //     nav.style.backgroundColor = '#eee9e6';
-    //     nav.style.color = '#83502e';
-    //     // navLogo.src = 'img/logo-sm-black.jpg';
-    //     logoText.style.color ='#83502e';
+    if (window.pageYOffset > 25){
+        // nav.style.transition = 'background-color 500ms ease-in-out';
+        nav.style.backgroundColor = '#1a1a1a';
+        nav.style.color = '#eee9e6';
+        // navLogo.src = 'img/logo-sm-black.jpg';
+        logoText.style.color ='#d2a56d';
+        logoTextLg.style.color ='#d2a56d';
 
-    // }
-    // if (window.pageYOffset < 26) {
-    //     // nav.style.transition = 'background-color 500ms ease-in-out';
-    //     nav.style.backgroundColor = 'rgba(0,0,0,0)';
-    //     nav.style.color = '';
-    //     // navLogo.src = 'img/logo-sm-white.jpg';
-    //     logoText.style.color ='#ffffffad';
-    // }
+    }
+    if (window.pageYOffset < 26) {
+        // nav.style.transition = 'background-color 500ms ease-in-out';
+        nav.style.backgroundColor = 'rgba(0,0,0,0)';
+        nav.style.color = '';
+        // navLogo.src = 'img/logo-sm-white.jpg';
+        logoText.style.color ='#ffffffad';
+    }
 });
